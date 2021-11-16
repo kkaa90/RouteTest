e33c3d40d73d3da080f9842b9c1acdf7e7a5bbbe
kkaa90 <kkaa90@naver.com>
2021-11-16 21:19:30 +0900

    211116 v1

--- app/src/main/java/com/e/routetest/ShowBoardActivity.java
+++ app/src/main/java/com/e/routetest/ShowBoardActivity.java
@@ -1,25 +1,40 @@
 package com.e.routetest;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
+import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
 
 import static com.e.routetest.LoadingActivity.sv;
 import static com.e.routetest.LoadingActivity.allSpotList;
+
+import static com.e.routetest.LoginActivity.userId;
 
 public class ShowBoardActivity extends AppCompatActivity {
 
@@ -61,15 +76,62 @@ public class ShowBoardActivity extends AppCompatActivity {
                 }
             }
         }.start();
+        Button uButton = (Button)findViewById(R.id.updateButton);
+        uButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        Button dButton = (Button)findViewById(R.id.deleteButton);
+        dButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new Thread(){
+                    public void run(){
+                        deleteBoard(boardId);
+                        finish();
+                    }
+                }.start();
+            }
+        });
+        Button jButton = (Button)findViewById(R.id.joinButton);
+        jButton.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View view) {
+                new Thread(){
+                    public void run(){
+                        if(joinAttempt(titleView1.getText().toString(),userView1.getText().toString())){
+                            AlertDialog.Builder builder = new AlertDialog.Builder(ShowBoardActivity.this);
+                            builder.setTitle("신청완료");
+                            builder.setMessage("신청이 완료되었습니다.");
+                            builder.setNeutralButton("확인", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    finish();
+                                }
+                            });
+                        }
+                        else {
+                            AlertDialog.Builder builder = new AlertDialog.Builder(ShowBoardActivity.this);
+                            builder.setTitle("오류발생");
+                            builder.setMessage("오류가 발생했습니다.");
+                            builder.setNeutralButton("확인", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+
+                                }
+                            });
+                        }
+                    }
+                }.start();
+            }
+        });
 
     }
-
-
-
-
     public boolean getBoard(int n){
-
-
         try {
             String url = sv + "getBoard.jsp?boardID="+n;
             System.out.println(url);
@@ -117,4 +179,84 @@ public class ShowBoardActivity extends AppCompatActivity {
         return false;
 
     }
+
+    public boolean deleteBoard(int n){
+        try {
+            String url = sv + "deleteBoard.jsp?boardID="+n;
+            System.out.println(url);
+
+
+            OkHttpClient client = new OkHttpClient();
+            Request.Builder builder = new Request.Builder().url(url).get();
+            Request request = builder.build();
+
+            Response response = client.newCall(request).execute();
+            Gson gson = new Gson();
+            JsonParser jsonParser = new JsonParser();
+            JsonElement jsonElement = jsonParser.parse(response.body().string());
+            JsonObject jsonObject = jsonElement.getAsJsonObject();
+            String success = jsonObject.get("success").getAsString();
+            return true;
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return false;
+    }
+    public boolean joinAttempt(String title, String writer){
+        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance("https://route-f81c2-default-rtdb.asia-southeast1.firebasedatabase.app/");
+        DatabaseReference databaseReference = firebaseDatabase.getReference();
+        String FCM_MESSAGE_URL = "https://fcm.googleapis.com/fcm/send";
+        String st = "AAAALpTg_A4:APA91bE_gvdBf4BT6Hm4r2oeGaJgdrbeLt_-j8tSiXrsh8kfH6ooigsCbaKqOo6GxrR0be6VyYsAS3vyzY5YSwgVcdj3RZEjwP5fKryDFbAMxdtXp_vazwHYlSzBuAYNALGuuqKsyJsX";
+        final String[] tt = {""};
+        databaseReference.child("users").child(writer).child("token").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                if(!task.isSuccessful()){
+                    System.out.println(task.getException().toString());
+                }
+                else{
+                    System.out.println(task.getResult().getValue().toString());
+                    tt[0] =task.getResult().getValue().toString();
+                }
+            }
+        });
+        while (tt[0].equals("")){
+            System.out.println("토큰 수신중");
+        }
+        System.out.println(tt[0]);
+
+
+        JsonObject jsonObject = new JsonObject();
+        JsonObject tJsonObject = new JsonObject();
+        Gson gson = new Gson();
+        JsonElement jsonElement = gson.toJsonTree(tt[0]);
+        JsonObject nJsonObject = new JsonObject();
+        jsonObject.add("to",jsonElement);
+        nJsonObject.addProperty("title","여행 참가신청");
+        nJsonObject.addProperty("body", "회원님이 작성하신 ["+title+"] 게시물에 ["+ userId+"] 회원님이 참가 신청 하셨습니다.");
+        jsonObject.add("notification",nJsonObject);
+
+        System.out.println(jsonObject.toString());
+        final MediaType mediaType = MediaType.parse("application/json; charset=utf-8");
+        OkHttpClient okHttpClient = new OkHttpClient();
+        try {
+            Request request = new Request.Builder().url(FCM_MESSAGE_URL)
+                    .addHeader("Content-Type","application/json; UTF-8")
+                    .addHeader("Authorization","key="+st)
+                    .post(RequestBody.create(mediaType, jsonObject.toString())).build();
+            Response response = okHttpClient.newCall(request).execute();
+
+
+            String res = response.toString();
+            System.out.println(res);
+            return true;
+        } catch (IOException e){
+            System.out.println(e);
+        }
+        return false;
+    }
+
 }
